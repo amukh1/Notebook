2947ab10e6c7d987f6a4d3968dc25ab0e81e598b
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-11-16 16:31:48 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+class Page {
+    constructor(pageName, desc) {
+        this.name = pageName;
+        this.desc = desc;
+        let page = `<a href="/page/${this.name}.html">
+        <div class="page">
+            <div class="pageName">
+${this.name}
+            </div>
+            <div class="pageDesc">
+${this.desc}
+            </div>
+        </div>
+    </a>`
+        document.getElementById("pages").innerHTML += page;
+    }
+}
+
+
+new Page("Trigonometry", "My notes on Trigonometry, and Tao/pi");
+new Page("Derivatives", "My notes on Derivatives and how they nice");
+new Page('Integrals','My notes on integrals and areas under curves');
+
+// let page = window.location.hash.slice(1)
+// let content = page;
+// let app = document.getElementById("content");
+// app.innerHTML = content;
